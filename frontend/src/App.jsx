6b4977fb277fdabe1b7e22fcd80ed80c785af4f9
akashdey23 <akashdey9128@gmail.com>
2025-07-08 00:33:52 +0530

    tailwind-css ready

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route } from 'react-router-dom'
+import Home from "./pages/Home"
+import Collection from "./pages/Collection"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Product from "./pages/Product"
+import Cart from "./pages/Cart"
+import Login from "./pages/Login"
+import Placeorder from "./pages/Placeorder"
+import Orders from "./pages/Orders"
+import Navbar from './components/Navbar'
+import React from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/collection' element={<Collection />}/>
+          <Route path='/about' element={<About />}/>
+          <Route path='/contact' element={<Contact />}/>
+          <Route path='/product/:productId' element={<Product />}/>
+          <Route path='/cart' element={<Cart />}/>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/placeorder' element={<Placeorder />}/>
+          <Route path='/orders' element={<Orders />}/>
+        </Routes>
+        <p className="text-3xl font-bold underline">
+          Hello world!
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
