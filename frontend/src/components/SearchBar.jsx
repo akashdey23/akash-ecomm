f2fdb7eaba6850ed32e78ed360fe3c45ea9ba472
akashdey23 <akashdey9128@gmail.com>
2025-07-09 19:48:29 +0530

    git

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,10 +1,24 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { assets } from '../assets/assets'
+import { useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 
 const SearchBar = () => {
   const {search, setSearch,  showSearch, setShowSearch} = useContext(ShopContext)
-    return showSearch ? (
+  const location = useLocation();
+  const [visible, setVisible] = useState(false);
+  useEffect(() => {
+    if(location.pathname.includes('collection' ) && showSearch){
+        setVisible(true);
+    }
+    else{
+        setVisible(false);
+    }
+    
+  }, [location,showSearch])
+  
+    return showSearch && visible ? (
     <div className='border-t border-b bg-gray-50 text-center'>
         <div className='inline-flex items-center border border-gray-400 justify-center px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2 md:'>
             <input type='text' placeholder='search' className='flex-1 outline-none bg-inherit text-sm' value={search} onChange={(e)=>(e.target.value)}/>
